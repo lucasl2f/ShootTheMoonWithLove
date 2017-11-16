1ead51e748d1d9126e0c683a2b5ab1491395163f
LucasL2F <lfonseca010@gmail.com>
2017-11-16 00:09:45 -0200

    Screen resolution: 450x800

--- Project/conf.lua
+++ Project/conf.lua
@@ -2,7 +2,7 @@
 function love.conf(t)
 	t.title = "Shoot the moon with love"
 	t.version = "0.10.2"
-	t.window.width = 400
+	t.window.width = 450
 	t.window.height = 800
 	t.modules.physics = false
 	t.console = true
